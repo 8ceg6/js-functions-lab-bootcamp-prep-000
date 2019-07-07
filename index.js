b0b6af89d2b95b2b1cfc0f07cda08f819f06c901
cedric e goddard <8ceg6@comcast.net>
2019-07-07 17:16:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // write your code below!
-let name= "you"
-let holiday= "Independence Day"
-let days="20"
+let name= "you";
+let holiday= "Independence Day";
+let days="20";
 function happyHolidays(){
   return "Happy holidays!";
 } 
